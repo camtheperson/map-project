2b1a412b830707ba6cc733612f56be2f87a960d5
Cameron Hermens <info@camtheperson.com>
2023-11-10 00:50:20 -0800

    Fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { MapComponent } from './components/MapComponent'
 export default function Home() {
     return (
         <main className="p-24">
-            <h1 className="text-3xl font-bold mb-5">Cam's Trails</h1>
+            <h1 className="text-3xl font-bold mb-5">Cam&apos;s Trails</h1>
             <MapComponent />
         </main>
     )
